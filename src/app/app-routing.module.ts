7682b941c16210cbf02423805f52ad3c5a938321
geeksceo <oowashina@gmail.com>
2024-03-18 02:02:15 +0100

    Board integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
+import { ChatsComponent } from './components/chats/chats/chats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'b', component: BoardComponent,}, // boards
+  {path:'c', component: ChatsComponent}, // chats
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { BoardComponent } from './components/board/board.component';
+import { ListComponent } from './components/list/list.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { ChatsComponent } from './components/chats/chats/chats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BoardComponent,
+    ListComponent,
+    TicketComponent,
+    ChatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // BrowserAnimationsModule,
+    // NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
